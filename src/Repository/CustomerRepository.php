58b66550e409b2dd5f68e17cf105167755905af7
Djordje Mancovic <djordjemancovic@Djordjes-MacBook-Pro-13.local>
2021-09-22 13:36:21 +0200

    Initial commit

---
+++ src/Repository/CustomerRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Customer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * @method Customer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Customer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Customer[]    findAll()
+ * @method Customer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CustomerRepository extends ServiceEntityRepository
+{
+    private $manager;
+
+    public function __construct
+    (
+        ManagerRegistry $registry,
+        EntityManagerInterface $manager
+    )
+    {
+        parent::__construct($registry, Customer::class);
+        $this->manager = $manager;
+    }
+
+    public function saveCustomer($firstName, $lastName, $email, $phoneNumber)
+    {
+        $newCustomer = new Customer();
+
+        $newCustomer
+            ->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setEmail($email)
+            ->setPhoneNumber($phoneNumber);
+
+        $this->manager->persist($newCustomer);
+        $this->manager->flush();
+    }
+
+    public function updateCustomer(Customer $customer): Customer
+    {
+        $this->manager->persist($customer);
+        $this->manager->flush();
+
+        return $customer;
+    }
+
+    public function removeCustomer(Customer $customer)
+    {
+        $this->manager->remove($customer);
+        $this->manager->flush();
+    }
+}
